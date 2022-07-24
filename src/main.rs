2da2a26d7ba0e2149f53be222103575f3655cafa
Margarita <margarita.kopniczky@genomicsplc.com>
2022-07-24 21:47:11 +0100

    try out process:Command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ferris_says::say; // from the previous step
 use std::io::{stdout, BufWriter};
+use std::process::Command;
 
 fn main() {
     let stdout = stdout();
@@ -9,5 +10,26 @@ fn main() {
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
 
-    println!("Hello World!");
+
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "echo hello"])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("echo hello")
+                .output()
+                .expect("failed to execute process")
+    };
+
+    let hello = output.stdout;
+    println!("{:?}", hello);
+    // TODO: format with
+    // use std::io::Write;
+
+    let mut list_dir = Command::new("ls");
+    // Execute `ls` in the current directory of the program.
+    list_dir.status().expect("process failed to execute");
 }
