0c4fa1ff63e4410e5f8491c804d33adccb0e8f7e
Margarita <margarita.kopniczky@genomicsplc.com>
2022-07-26 14:03:26 +0100

    decode bytes to print hello and delete the rustacean

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,19 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
 use std::process::Command;
+use std::io::{self, Write};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
-
-
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-                .args(["/C", "echo hello"])
-                .output()
-                .expect("failed to execute process")
-    } else {
+    let output =
         Command::new("sh")
                 .arg("-c")
                 .arg("echo hello")
                 .output()
-                .expect("failed to execute process")
-    };
-
+                .expect("failed to execute process");
     let hello = output.stdout;
     println!("{:?}", hello);
-    // TODO: format with
-    // use std::io::Write;
+    let mut stdout = io::stdout().lock();
+    stdout.write_all(&hello);
+
 
     let mut list_dir = Command::new("ls");
     // Execute `ls` in the current directory of the program.
